af6baf6ebaf2561f8fbf63aac00d6ac94f3de5cf
Vishva <gvishva2601@gmail.com>
2022-12-05 22:48:02 +0530

    Dispaly Default Value Primitive Data Types

---
+++ src/DisplayDefaultValuePrimitiveDataType_P2.java
@@ -0,0 +1,25 @@
+public class DisplayDefaultValuePrimitiveDataType_P2
+{
+    //declared help of static Keyword to StaticVariable
+    static byte b  ;
+    static short s;
+    static int i;
+    static float f;
+    static long l;
+    static double d;
+    static char c;
+    static boolean b1;
+
+    public static void main(String[] args)
+    {
+        //assigning variable
+        System.out.println("Byte Default Value is    :- " +b);
+        System.out.println("Short Default Value is   :- " +s);
+        System.out.println("Integer Default Value is :- " +i);
+        System.out.println("Float Default Value is   :- " +f);
+        System.out.println("Long Default Value is    :- " +l);
+        System.out.println("Double Default Value is  :- " +d);
+        System.out.println("Char Default Value is    :- " +c);
+        System.out.println("Boolean Default Value is :- " +b1);
+    }
+}
