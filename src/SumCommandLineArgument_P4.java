6b3a9758214545c78b6eaceaa4b8f23407d4b239
Vishva <gvishva2601@gmail.com>
2022-12-05 22:50:13 +0530

    Sum Of Numbers Using Command Line Argument

---
+++ src/SumCommandLineArgument_P4.java
@@ -0,0 +1,12 @@
+public class SumCommandLineArgument_P4
+{
+    public static void main(String[] args)
+    {
+        int s=0;
+        for(int i=0; i<args.length; i++)
+        {
+            s=s+Integer.parseInt(args[i]);
+        }
+        System.out.println("Sum = " +s);
+    }
+}
