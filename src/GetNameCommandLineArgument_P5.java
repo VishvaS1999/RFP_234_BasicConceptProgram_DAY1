f13d6e4c9d43d639630a027933e71606083a3e8b
Vishva <gvishva2601@gmail.com>
2022-12-05 22:51:19 +0530

    Get Name Command Line Argument

---
+++ src/GetNameCommandLineArgument_P5.java
@@ -0,0 +1,9 @@
+public class GetNameCommandLineArgument_P5
+{
+    public static void main(String[] args)
+    {
+        for(int i=0; i<args.length; i++)
+            System.out.println(args[i]);
+
+    }
+}
