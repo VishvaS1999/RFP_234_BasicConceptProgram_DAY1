1543effaac02fe41bcede8d1cd6c30f92f663ba0
Vishva <gvishva2601@gmail.com>
2022-12-05 22:47:06 +0530

    Display Any Message

---
+++ src/DisplayAnyMessage_P1.java
@@ -0,0 +1,9 @@
+public class DisplayAnyMessage_P1
+{
+    public static void main(String[] args)
+    {
+        //Printing The Message
+        System.out.println("Hello, \nWelcome To The Java Program!...");
+
+    }
+}
